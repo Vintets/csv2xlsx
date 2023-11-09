b83400ddbce1c689fc268742c716dea5492dcdbc
Vintets <vintets@mail.ru>
2023-11-09 13:02:12 +0300

    consider ADDITIONAL_ACTIONS шт hidden_columns

--- csv2xlsx.py
+++ csv2xlsx.py
@@ -141,7 +141,7 @@ class Excel:
                     self.ws.delete_cols(cell.column)
 
     def hidden_columns(self) -> None:
-        if not config.HIDDEN_COLUMNS:
+        if not (config.ADDITIONAL_ACTIONS and config.HIDDEN_COLUMNS):
             return
         for row_cells in self.ws.iter_rows(min_row=1, max_row=1):
             for cell in reversed(row_cells):
